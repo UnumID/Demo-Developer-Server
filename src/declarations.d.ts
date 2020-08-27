0fbdaa2befb0ebd422beaddaa338b479dec9788d
jacobleesinger <jacobleesinger@gmail.com>
2020-08-27 11:56:23 -0700

    add CompanyService

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,6 @@
 import { Application as ExpressFeathers } from '@feathersjs/express';
-import { MikroORM, EntityManager } from 'mikro-orm';
+import { MikroORM, EntityManager, EntityRepository } from 'mikro-orm';
+import { Company } from './entities/Company';
 
 // A mapping of service names to types. Will be extended in service files.
 export interface ServiceTypes {} // eslint-disable-line @typescript-eslint/no-empty-interface
@@ -7,6 +8,7 @@ export interface ServiceTypes {} // eslint-disable-line @typescript-eslint/no-em
 export interface Mikro {
   orm: MikroORM;
   em: EntityManager;
+  companyRepository: EntityRepository<Company>;
 }
 
 // The application instance type that will be used everywhere else
--- src/initMikroOrm.ts
+++ src/initMikroOrm.ts
@@ -5,6 +5,7 @@ import {
 
 import { Application, Mikro } from './declarations';
 import config from './mikro-orm.config';
+import { Company } from './entities/Company';
 
 export const mikro = {} as Mikro;
 
@@ -12,6 +13,7 @@ export async function initMikroOrm (app: Application): Promise<void> {
   mikro.orm = await MikroORM.init(config);
 
   mikro.em = mikro.orm.em;
+  mikro.companyRepository = mikro.em.getRepository(Company);
 
   app.use((req, res, next) => RequestContext.create(mikro.orm.em, next));
   app.mikro = mikro; // eslint-disable-line no-param-reassign
