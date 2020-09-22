122ce55a20cd7929671f6a5068493a71eb4356a7
jacobleesinger <jacobleesinger@gmail.com>
2020-09-21 18:32:48 -0700

    add logs to all requests

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -2,9 +2,33 @@
 import { HookContext } from '@feathersjs/feathers';
 import { pick } from 'lodash';
 
+import logger from './logger';
+
+function logRequest (ctx: HookContext): void {
+  const { path, method, id } = ctx;
+  logger.info(`${path}#${method}${id ? ` id: ${id}` : ''}`);
+}
+
+function logError (ctx: HookContext): void {
+  const { path, method, error } = ctx;
+  const { name, code, message } = error;
+  const rawInfo = pick(ctx, ['params', 'id', 'data']);
+  const info = { ...rawInfo };
+
+  if (info.params?.headers?.authorization) {
+    info.params.headers.authorization = 'Bearer *****';
+  }
+
+  if (info.params.authToken) {
+    info.params.authToken = '*****';
+  }
+
+  logger.warn(`Error in ${path}#${method}: name=${name} code=${code} message=${message}`, info);
+}
+
 export default {
   before: {
-    all: [],
+    all: [logRequest],
     find: [],
     get: [],
     create: [],
@@ -24,12 +48,7 @@ export default {
   },
 
   error: {
-    all: [(ctx: HookContext): void => {
-      const { path, method, error } = ctx;
-      const { name, code, message } = error;
-      const info = pick(ctx, ['path', 'method', 'params', 'id', 'data']);
-      console.warn(`Error in ${path}#${method}: name=${name} code=${code} message=${message}`, info);
-    }],
+    all: [logError],
     find: [],
     get: [],
     create: [],
