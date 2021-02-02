9557b2b32cb94d8ad5ea95e3b3bdb2cebe7e7a09
Ray Smets <rayjsmets@gmail.com>
2021-02-01 19:43:55 -0800

    [PresentationServiceV2] working.

--- src/services/PresentationService.ts
+++ src/services/PresentationService.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import { Application } from '../declarations';
 import { config } from '../config';
-import { PresentationOrNoPresentation, Presentation, NoPresentation, IssuerInfoMap } from '../types';
+import { PresentationOrNoPresentation, Presentation, NoPresentation, IssuerInfoMap, EncryptedData } from '../types';
 import logger from '../logger';
 import { Channel } from '@feathersjs/transport-commons/lib/channels/channel/base';
 
@@ -22,9 +22,14 @@ export interface VerificationResponse {
   data: PresentationReceiptInfo;
 }
 
-export function isPresentation (presentation: PresentationOrNoPresentation): presentation is Presentation {
-  return presentation.type[0] === 'VerifiablePresentation';
+export interface EncryptedPresentation {
+  presentationRequestUuid: string;
+  encryptedPresentation: EncryptedData;
 }
+
+// export function isPresentation (presentation: EncryptedPresentation): presentation is Presentation {
+//   return presentation.type[0] === 'VerifiablePresentation';
+// }
 
 export function publisher (app: Application) {
   return async function actualPublisher (response: any): Promise<Channel> {
@@ -48,7 +53,7 @@ export class PresentationService {
     const verifierService = this.app.service('verifier');
 
     // verify presentation
-    const url = `${config.VERIFIER_URL}/api/verifyPresentation`;
+    const url = `${config.VERIFIER_URL}/api/verifyEncryptedPresentation`;
     const headers = { Authorization: `Bearer ${verifier.authToken}` };
 
     const presentationReceiptInfo: PresentationReceiptInfo = {
@@ -57,21 +62,21 @@ export class PresentationService {
       holderApp: presentationRequest.holderApp.uuid
     };
 
-    // for now, assume all NoPresentations are valid
-    // TODO: remove or replace with actual implementation once Verifier-Server-App is updated
-    // to handle NoPresentations (https://trello.com/c/DbvobNVo/612-handle-nopresentations-part-2)
-    if (!isPresentation(presentation)) {
-      logger.info('Received NoPresentation', presentation);
+    // // for now, assume all NoPresentations are valid
+    // // TODO: remove or replace with actual implementation once Verifier-Server-App is updated
+    // // to handle NoPresentations (https://trello.com/c/DbvobNVo/612-handle-nopresentations-part-2)
+    // if (!isPresentation(presentation)) {
+    //   logger.info('Received NoPresentation', presentation);
 
-      const noPresentationUrl = `${config.VERIFIER_URL}/api/verifyNoPresentation`;
+    //   const noPresentationUrl = `${config.VERIFIER_URL}/api/verifyNoPresentation`;
 
-      const response = await axios.post(noPresentationUrl, { noPresentation: presentation, verifier: verifier.did }, { headers });
+    //   const response = await axios.post(noPresentationUrl, { noPresentation: presentation, verifier: verifier.did }, { headers });
 
-      if (!response.data.isVerified) {
-        throw new BadRequest('Verification failed.');
-      }
-      return { isVerified: response.data.isVerified, type: 'NoPresentation', data: presentationReceiptInfo };
-    }
+    //   if (!response.data.isVerified) {
+    //     throw new BadRequest('Verification failed.');
+    //   }
+    //   return { isVerified: response.data.isVerified, type: 'NoPresentation', data: presentationReceiptInfo };
+    // }
 
     // forward request to verifier
     const response = await axios.post(url, { presentation, verifier: verifier.did }, { headers });
@@ -90,26 +95,26 @@ export class PresentationService {
       throw new BadRequest('Verification failed');
     }
 
-    // save shared credentials
-    const sharedCredentialService = this.app.service('sharedCredential');
-    const issuerService = this.app.service('issuer');
-    const userService = this.app.service('user');
+    // // save shared credentials
+    // const sharedCredentialService = this.app.service('sharedCredential');
+    // const issuerService = this.app.service('issuer');
+    // const userService = this.app.service('user');
 
-    for (const credential of presentation.verifiableCredential) {
-      // get saved issuer and user by their dids
-      // note that the saved dids will not include key identifier fragments, which may be included in the credential
-      const issuer = await issuerService.get(null, { where: { did: credential.issuer.split('#')[0] } });
-      const user = await userService.get(null, { where: { did: credential.credentialSubject.id.split('#')[0] } });
+    // for (const credential of presentation.verifiableCredential) {
+    //   // get saved issuer and user by their dids
+    //   // note that the saved dids will not include key identifier fragments, which may be included in the credential
+    //   const issuer = await issuerService.get(null, { where: { did: credential.issuer.split('#')[0] } });
+    //   const user = await userService.get(null, { where: { did: credential.credentialSubject.id.split('#')[0] } });
 
-      const options = {
-        verifierUuid: verifier.uuid,
-        issuerUuid: issuer.uuid,
-        userUuid: user.uuid,
-        credential
-      };
+    //   const options = {
+    //     verifierUuid: verifier.uuid,
+    //     issuerUuid: issuer.uuid,
+    //     userUuid: user.uuid,
+    //     credential
+    //   };
 
-      await sharedCredentialService.create(options);
-    }
+    //   await sharedCredentialService.create(options);
+    // }
 
     return { isVerified: true, type: 'VerifiablePresentation', data: presentationReceiptInfo };
   }
--- src/services/PresentationServiceV2.ts
+++ src/services/PresentationServiceV2.ts
@@ -4,25 +4,32 @@ import axios from 'axios';
 
 import { Application } from '../declarations';
 import { config } from '../config';
-import { PresentationOrNoPresentation, Presentation, NoPresentation } from '../types';
+import { PresentationOrNoPresentation, Presentation, NoPresentation, IssuerInfoMap, EncryptedData } from '../types';
 import logger from '../logger';
 import { Channel } from '@feathersjs/transport-commons/lib/channels/channel/base';
 
 export interface PresentationReceiptInfo {
-    subjectDid: string;
-    issuers: [string];
-    verifier: string;
-  }
+  subjectDid: string;
+  verifierDid: string;
+  holderApp: string;
+  credentialTypes?: [string];
+  issuers?: IssuerInfoMap;
+}
 
 export interface VerificationResponse {
   isVerified: boolean;
   type: 'VerifiablePresentation' | 'NoPresentation';
-  data: PresentationReceiptInfo
+  data: PresentationReceiptInfo;
 }
 
-export function isPresentation (presentation: PresentationOrNoPresentation): presentation is Presentation {
-  return presentation.type[0] === 'VerifiablePresentation';
+export interface EncryptedPresentation {
+  presentationRequestUuid: string;
+  encryptedPresentation: EncryptedData;
 }
+
+// export function isPresentation (presentation: EncryptedPresentation): presentation is Presentation {
+//   return presentation.type[0] === 'VerifiablePresentation';
+// }
 
 export function publisher (app: Application) {
   return async function actualPublisher (response: any): Promise<Channel> {
@@ -46,24 +53,31 @@ export class PresentationServiceV2 {
     const verifierService = this.app.service('verifier');
 
     // verify presentation
+    // const url = `${config.VERIFIER_URL}/api/verifyEncryptedPresentation`;
     const url = `${config.VERIFIER_URL}/api/verifyPresentation`;
     const headers = { Authorization: `Bearer ${verifier.authToken}` };
 
-    // for now, assume all NoPresentations are valid
-    // TODO: remove or replace with actual implementation once Verifier-Server-App is updated
-    // to handle NoPresentations (https://trello.com/c/DbvobNVo/612-handle-nopresentations-part-2)
-    if (!isPresentation(presentation)) {
-      logger.info('Received NoPresentation', presentation);
+    const presentationReceiptInfo: PresentationReceiptInfo = {
+      subjectDid: presentationRequest.metadata?.userUuid,
+      verifierDid: verifier.did,
+      holderApp: presentationRequest.holderApp.uuid
+    };
 
-      const noPresentationUrl = `${config.VERIFIER_URL}/api/verifyNoPresentation`;
+    // // for now, assume all NoPresentations are valid
+    // // TODO: remove or replace with actual implementation once Verifier-Server-App is updated
+    // // to handle NoPresentations (https://trello.com/c/DbvobNVo/612-handle-nopresentations-part-2)
+    // if (!isPresentation(presentation)) {
+    //   logger.info('Received NoPresentation', presentation);
 
-      const response = await axios.post(noPresentationUrl, { noPresentation: presentation, verifier: verifier.did }, { headers });
+    //   const noPresentationUrl = `${config.VERIFIER_URL}/api/verifyNoPresentation`;
 
-      if (!response.data.isVerified) {
-        throw new BadRequest('Verification failed.');
-      }
-      return { isVerified: response.data.isVerified, type: 'NoPresentation', data: presentation };
-    }
+    //   const response = await axios.post(noPresentationUrl, { noPresentation: presentation, verifier: verifier.did }, { headers });
+
+    //   if (!response.data.isVerified) {
+    //     throw new BadRequest('Verification failed.');
+    //   }
+    //   return { isVerified: response.data.isVerified, type: 'NoPresentation', data: presentationReceiptInfo };
+    // }
 
     // forward request to verifier
     const response = await axios.post(url, { presentation, verifier: verifier.did }, { headers });
@@ -82,28 +96,28 @@ export class PresentationServiceV2 {
       throw new BadRequest('Verification failed');
     }
 
-    // save shared credentials
-    const sharedCredentialService = this.app.service('sharedCredential');
-    const issuerService = this.app.service('issuer');
-    const userService = this.app.service('user');
+    // // save shared credentials
+    // const sharedCredentialService = this.app.service('sharedCredential');
+    // const issuerService = this.app.service('issuer');
+    // const userService = this.app.service('user');
 
-    for (const credential of presentation.verifiableCredential) {
-      // get saved issuer and user by their dids
-      // note that the saved dids will not include key identifier fragments, which may be included in the credential
-      const issuer = await issuerService.get(null, { where: { did: credential.issuer.split('#')[0] } });
-      const user = await userService.get(null, { where: { did: credential.credentialSubject.id.split('#')[0] } });
+    // for (const credential of presentation.verifiableCredential) {
+    //   // get saved issuer and user by their dids
+    //   // note that the saved dids will not include key identifier fragments, which may be included in the credential
+    //   const issuer = await issuerService.get(null, { where: { did: credential.issuer.split('#')[0] } });
+    //   const user = await userService.get(null, { where: { did: credential.credentialSubject.id.split('#')[0] } });
 
-      const options = {
-        verifierUuid: verifier.uuid,
-        issuerUuid: issuer.uuid,
-        userUuid: user.uuid,
-        credential
-      };
+    //   const options = {
+    //     verifierUuid: verifier.uuid,
+    //     issuerUuid: issuer.uuid,
+    //     userUuid: user.uuid,
+    //     credential
+    //   };
 
-      await sharedCredentialService.create(options);
-    }
+    //   await sharedCredentialService.create(options);
+    // }
 
-    return { isVerified: true, type: 'VerifiablePresentation', data: presentation };
+    return { isVerified: true, type: 'VerifiablePresentation', data: presentationReceiptInfo };
   }
 
   setup (app: Application): void {
@@ -118,7 +132,7 @@ declare module '../declarations' {
 }
 
 export default function (app: Application): void {
-  app.use('/v2/presentation', new PresentationServiceV2());
-  const service = app.service('presentation');
+  app.use('/presentationV2', new PresentationServiceV2());
+  const service = app.service('presentationV2');
   service.publish(publisher(app));
 }
